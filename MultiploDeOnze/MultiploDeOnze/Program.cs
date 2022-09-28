0cba22a053a0847e8c2eaa2b4bca0ccb5916a351
Fábio Empadinhas <f.empadinhas@outlook.com>
2022-09-28 15:18:21 +0100

    Configuration Inicial (DI, Serilog, Build)

--- MultiploDeOnze/MultiploDeOnze/Program.cs
+++ MultiploDeOnze/MultiploDeOnze/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace MultiploDeOnze
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			var builder = new ConfigurationBuilder();
+			BuildConfig(builder);
+			//Configurar o Serilog
+			Log.Logger = new LoggerConfiguration()
+			.ReadFrom.Configuration(builder.Build())
+			.Enrich.FromLogContext()
+			.WriteTo.Console()
+			.CreateLogger();
+
+			Log.Logger.Information("App MultiploOnze Starting");
+		}
+		//Configurar o Builder
+		static void BuildConfig(IConfigurationBuilder builder)
+		{
+			builder.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile("appsettings.{Environment.GetEnvironmentVariable(\"ASPNETCORE_ENVIRONMENT\") ?? \"Production\"}.json", optional: false, reloadOnChange: true)
+			.AddEnvironmentVariables();
+		}
+	}
+}
