0cd8a93280ddf33249886b4bc50bde98fcde61b5
Fábio Empadinhas <f.empadinhas@outlook.com>
2022-09-28 19:54:07 +0100

    Deixados para tras no ultimo comit

---
+++ MultiploDeOnze/MultiploDeOnze/Data/ApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MultiploDeOnze.Models;
+
+namespace MultiploDeOnze.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Numeros> Numeros { get; set; }
+           public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
